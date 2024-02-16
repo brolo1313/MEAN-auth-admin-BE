12eafb13dc79ad81a1f307fc81691615adfb85d2
Andrew Yupin <brolo1341@gmail.com>
2024-02-16 14:38:49 +0200

    refactoring using express and ejs technologies

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,46 +1,31 @@
-const fs = require("fs");
+const express = require("express");
 const path = require("path");
 
-const http = require("http");
+const app = express();
+
+
+app.set('view engine', 'ejs');
 
 const host = "localhost";
 const port = 7000;
 
-const server = http.createServer(function (req, res) {
-  res.setHeader("Content-Type", "text/html");
+const createPath = (page) => path.resolve(__dirname, "ejs-view", `${page}.ejs`);
 
-  const createPath = (page) => path.resolve(__dirname, "views", `${page}.html`);
-  let basePath = '';
-
-  switch (req.url) {
-    case "/":
-    case "/home":
-      basePath = createPath("homepage");
-      res.statusCode = 200;
-      break;
-
-    default:
-      basePath = createPath("error");
-      res.statusCode = 404;
-      break;
-  }
-
-
-  console.log(basePath);
-  fs.readFile(basePath, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.statusCode = 500;
-      res.end();
-    } else {
-      res.write(data);
-      res.end();
-    }
-  });
-});
-
-server.listen(port, host, (error) => {
+app.listen(port, host, (error) => {
   error
     ? console.log(error)
     : console.log(`Server listens http://${host}:${port}`);
+});
+
+
+app.get(["/", "/home"], (req, res) => {
+  res.render(createPath("homepage"));
+});
+
+app.get("/login", (req, res) => {
+    res.render(createPath("login"));
+  });
+
+app.use((req, res) => {
+  res.status(404).render(createPath("error"));
 });
