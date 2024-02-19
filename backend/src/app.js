a1f3f0c5cabc5abfbb4e8771b9e821d3a8d4f90e
Andrew Yupin <brolo1341@gmail.com>
2024-02-19 10:26:30 +0200

    added skeleton, refactored code

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const morgan = require("morgan");
 const app = express();
 
-const homeMocks = require("./ejs-view/helpers/mocks");
+const homeMocksData = require("./ejs-view/helpers/mocks");
 const createPath = require("./ejs-view/helpers/helper");
 
 const host = "localhost";
@@ -21,16 +21,24 @@ app.listen(port, host, (error) => {
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms")
 );
+app.use(express.static(path.join(__dirname, "./ejs-view/helpers/data.json")));
 app.use(express.static(path.join(__dirname, "styles")));
 
 //API
 app.get(["/", "/home"], (req, res) => {
-  res.render(createPath("homepage"), { homeMocks: homeMocks });
+  const homeMocks = homeMocksData;
+  const title = "Home Page";
+
+  res.render(createPath("homepage"), { homeMocks, title });
 });
 
 app.get("/login", (req, res) => {
-  res.render(createPath("login"));
+  res.render(createPath("login"), { title: "Login Page" });
 });
+
+app.get("/data.json", (req, res) => {
+    res.sendFile(path.join(__dirname, "./ejs-view/helpers/data.json"));
+  });
 
 app.use((req, res) => {
   res.status(404).render(createPath("error"));
